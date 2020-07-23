b39ba68094e76dc21f9a3ccf3e13d1964c6291e9
TDL2627 <the.dark.lord.2627@gmail.com>
2020-07-24 01:48:28 +0200

    sign up change

--- signup.php
+++ signup.php
@@ -1,155 +1,66 @@
-<?php
-
-require_once "db/db.php";
- 
-
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-       
-        $sql = "SELECT id FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-         
-            $param_username = trim($_POST["username"]);
-            
-        
-            if(mysqli_stmt_execute($stmt)){
-               
-                mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-         
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-   
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have at least 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-    
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
-      
-        $sql = "INSERT INTO users ('username', 'password') VALUES (?, ?)";
-       
-         
-        if($stmt = mysqli_prepare($link, $sql)){
-          
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-            
-            
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); 
-            
-            
-            if(mysqli_stmt_execute($stmt)){
-                
-                header("location: index.php");
-            } else{
-                echo "Something went wrong. Please try again later.";
-            }
-
-           
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-
-    mysqli_close($link);
-}
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="css/stylesheet.css">
-        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"> 
-</head>
-<body>
-    
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">QUICK LINKS</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="login.php">LOGIN <span class="sr-only">(current)</span></a>
-            </li>
-    </ul> 
-    </nav>
-    <div class="log">
-    <h1>NATE'S ONLINE LAPTOP STORE</h1>
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-    
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary"  value="Submit" ><a href= "index.php">
-                <input type="reset" class="btn btn-default" value="Reset">
-            </div>
-            
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+<html lang="en-US">
+  <head>
+  <title>IT SourceCode</title>
+  <link rel="stylesheet" href="libs/css/bootstrap.min.css">
+  <link rel="stylesheet" href="libs/style.css">
+  </head>
+  <h1>Register</h1>
+<div class="reg-input-field">
+        <h3>Please Fill-out All Fields</h3>
+        <form method="post" action="#" >
+          <div class="form-group">
+            <label>Fullname</label>
+            <input type="text" class="form-control" name="fname" style="width:20em;" placeholder="Enter your Fullname" required />
+          </div>
+          <div class="form-group">
+            <label>Gender</label>
+            <input type="text" class="form-control" name="gender" style="width:20em;" placeholder="Enter your Gender" required pattern="[a-zA-Z .]+" />
+          </div>
+          <div class="form-group">
+            <label>Age</label>
+            <input type="number" class="form-control" name="age" style="width:20em;" placeholder="Enter your Age">
+          </div>
+          <div class="form-group">
+            <label>Address</label>
+            <input type="text" class="form-control" name="address" style="width:20em;" required placeholder="Enter your Address"></textarea>
+          </div>
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" class="form-control" name="user" style="width:20em;" placeholder="Enter your Username">
+          </div><div class="form-group">
+            <label>Password</label>
+            <input type="Password" class="form-control" name="pass" style="width:20em;" required  placeholder="Enter your Password">
+          </div>
+          <div class="form-group">
+            <input type="submit" name="submit" class="btn btn-primary submitBtn" style="width:20em; margin:0;" /><br><br>
+             <center>
+             <a href="index.php">LogIn</a>
+           </center>
+          </div>
         </form>
-    </div>  
-    <h3>DEVELOPED BY TDL2627</h3>  
-</body>
-</html>
+      </div>
+      </html>
+      <?php
+      include 'db/db.php';
+if(isset($_POST['submit'])){
+$fname = $_POST['fname'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$address = $_POST['address'];
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+
+  $query = "INSERT INTO users
+                (username, password, full_name,gender,age,address)
+                VALUES ('".$user."','".$pass."','".$fname."','".$gender."','".$age."','".$address."')";
+                mysqli_query($db,$query)or die ('Error in updating Database');
+                ?>
+                <script type="text/javascript">
+            alert("Successfull Added.");
+            window.location = "index.php";
+        </script>
+                <?php
+}
+      ?>
