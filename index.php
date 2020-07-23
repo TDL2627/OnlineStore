cd7d8afe046594c5b4e1b91dd4c8a84c1e3cb468
TDL2627 <the.dark.lord.2627@gmail.com>
2020-07-23 13:48:38 +0200

    debug

--- index.php
+++ index.php
@@ -79,7 +79,38 @@ if(empty($_SESSION["shopping_cart"])) {
  }
 }
 ?>
-    
+  <?php
+if(!empty($_SESSION["shopping_cart"])) {
+$cart_count = count(array_keys($_SESSION["shopping_cart"]));
+?>
+<div class="cart_div">
+<a href="cart.php"><img src="cart-icon.png" /> Cart<span>
+<?php echo $cart_count; ?></span></a>
+</div>
+<?php
+}
+?>
+<?php
+$result = mysqli_query($con,"SELECT * FROM `products`");
+while($row = mysqli_fetch_assoc($result)){
+    echo "<div class='product_wrapper'>
+    <form method='post' action=''>
+    <input type='hidden' name='code' value=".$row['code']." />
+    <div class='image'><img src='".$row['image']."' /></div>
+    <div class='name'>".$row['name']."</div>
+    <div class='price'>$".$row['price']."</div>
+    <button type='submit' class='buy'>Buy Now</button>
+    </form>
+    </div>";
+        }
+mysqli_close($con);
+?>
+ 
+<div style="clear:both;"></div>
+ 
+<div class="message_box" style="margin:10px 0px;">
+<?php echo $status; ?>
+</div>
     <h3>DEVELOPED BY TDL2627</h3>
 </body>
 </html>
